7cca7badf2c7c899df982b99054596c890e91467
Nicolas Ayala <roacutanico@gmail.com>
2023-08-21 10:43:23 -0400

    si senhor

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import { Toaster } from '@/components/ui/toaster'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Login from './login'
 
 const inter = Inter({ subsets: ['latin'] })
 
