fd3f2c02ca39d9e7e18c18a5ed6e12543a67ec46
Nicolas Ayala <roacutanico@gmail.com>
2023-09-21 23:23:57 -0400

    last test

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -14,12 +14,12 @@ export async function GET(request: NextRequest) {
         const supabase = createRouteHandlerClient<any>({ cookies })
         await supabase.auth.exchangeCodeForSession(code)
         const { data: activeSession } = await supabase.auth.getSession();
-        const { data, error } = await supabase.rpc("tienda_by_user_id", {
+        const { data, error } = await supabase.rpc("get_store_by_user_id", {
             user_id: activeSession.session?.user.id
         });
         console.log("Data acá: ", data);
         console.log("Error acá: ", error);
-        return NextResponse.redirect(`/${data.url}`)
+        return NextResponse.redirect(`${requestUrl.origin}/${data.url}`)
     }
 
     // URL to redirect to after sign in process completes
