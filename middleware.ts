7985d8dc8c9d62b6553c167ec2376efd3366b18c
Nicolas Ayala <roacutanico@gmail.com>
2023-09-21 21:58:28 -0400

    Testing my middleware

--- middleware.ts
+++ middleware.ts
@@ -4,26 +4,25 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
+
     const res = NextResponse.next()
     const supabase = createMiddlewareClient({ req, res })
 
     const { data: { user } } = await supabase.auth.getUser()
 
-    // if user is signed in and the current path is / redirect the user to /account
     if (user) {
         console.log("Ya existe usuario");
-        return NextResponse.redirect(new URL('/manage-login', req.url))
+        return NextResponse.redirect(new URL('/holaquetal', req.url))
     }
 
-    // if user is not signed in and the current path is not / redirect the user to /
     if (!user && req.nextUrl.pathname !== '/') {
         console.log("No existe usuario");
-        return NextResponse.redirect(new URL('/', req.url))
+        return NextResponse.redirect(new URL('/quetalhola', req.url))
     }
 
     return res
 }
 
 export const config = {
-    matcher: ['/'],
+    matcher: ['/', '/manage-login'],
 }
