b7d77ad3e94f59d6ce4d1027c68f147aabd99b22
Nicolas Ayala <roacutanico@gmail.com>
2023-09-21 22:46:39 -0400

    asd

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,25 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
+
     const res = NextResponse.next()
     const supabase = createMiddlewareClient({ req, res })
-    await supabase.auth.getSession()
+
+    const { data: { user } } = await supabase.auth.getUser()
+
+    if (user) {
+        console.log("Ya existe usuario");
+        return NextResponse.redirect(new URL('/holaquetal', req.url))
+    }
+
+    if (!user) {
+        console.log("No existe usuario");
+        return NextResponse.redirect(new URL('/quetalhola', req.url))
+    }
+
     return res
+}
+
+export const config = {
+    matcher: ['/', '/login'],
 }
