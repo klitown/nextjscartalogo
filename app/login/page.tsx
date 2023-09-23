f56e359a5c1f1751f00c175836522e64c2d335f2
Nicolas Ayala <roacutanico@gmail.com>
2023-09-23 16:59:13 -0400

    he

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -98,10 +98,7 @@ export default function AuthForm() {
                                         }
                                     }}
                                     theme="light"
-
-                                    onlyThirdPartyProviders
                                     providers={['google']}
-                                    // redirectTo="https://cartalogo.digital/manage-login"
                                     localization={{
                                         variables: {
                                             sign_in: {
