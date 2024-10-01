1f335e8085bf55d5074ae76743bf366793809c86
Nicolas Ayala <roacutanico@gmail.com>
2024-09-30 20:03:27 -0400

    Update footer

--- app/(tienda)/[slug]/(infoTienda)/layout.tsx
+++ app/(tienda)/[slug]/(infoTienda)/layout.tsx
@@ -71,7 +71,7 @@ export default async function Layout({
                         </div>
 
                         <p className="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
-                            Copyright &copy; 2023 - 🇵🇾
+                            Copyright &copy; 2024 - 🇵🇾
                         </p>
                     </div>
                 </div>
